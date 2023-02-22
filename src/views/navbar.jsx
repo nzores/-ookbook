f83ceb083dfac91dec91abc203c7a50adf63c87a
nzores <akabardaev@gmail.com>
2023-02-22 12:10:51 +0300

    separate navbar

--- src/views/navbar.jsx
+++ src/views/navbar.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const React = require('react');
+const Layout = require('./Layout');
 
-function Layout(props) {
+module.exports = function Layout(props) {
   const { children } = props;
   return (
     <html lang="en">
@@ -15,6 +16,24 @@ function Layout(props) {
         <title>Document</title>
       </head>
       <body>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">Main</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/form">Form</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         {children}
       </body>
     </html>
