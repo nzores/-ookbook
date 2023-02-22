8dd40c755b4157661bb62950169afb8412bd7244
sucelius-thinkPad <sucelius@yandex.ru>
2023-02-22 16:28:07 +0300

    connect api

--- src/controllers/MainControllers.js
+++ src/controllers/MainControllers.js
@@ -1,6 +1,24 @@
 const render = require('../lib/renderTemplate');
 const Main = require('../views/Main');
+require('dotenv').config();
 
-exports.MainPage = (req, res) => {
+exports.MainPage = async (req, res) => {
+
+//сюда нужно передавтаь свой ключ для доступа в апи
+  const myApiKey = process.env.MY_API_KEY
+  //тут нужно указать сколько рецептов мы хотим получить за один запрос
+  const itemsPerRequest = 10
+//запрос к апи
+  const response = await fetch(`https://api.spoonacular.com/recipes/random?number=${20}&apiKey=${myApiKey}`, {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
+  });
+
+  // записываем результат ответа от апи
+  const data = await response.json();
+
+  console.log(data)
+
+
   render(Main, {}, res);
 };
