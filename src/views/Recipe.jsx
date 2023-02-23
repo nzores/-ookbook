bcaf075c9ac7a0f7878f6df1577431adfc316f48
sucelius-thinkPad <sucelius@yandex.ru>
2023-02-23 17:03:39 +0300

    add style to recipe page

--- src/views/Recipe.jsx
+++ src/views/Recipe.jsx
@@ -7,11 +7,12 @@ function Recipe({ oneRecipeData,igredients , username, userid}) {
   return (
     <Layout username={username} userid={userid}>
 
-        <div class="container-md">
+        <div class="container-fluid">
+        <div class="card mx-auto col-md-6 col-10 mt-5 pt-4">
         <h1 className="food">{oneRecipeData[0].title}</h1>
           <i className="fa fa-clock-o">cooking time : { oneRecipeData[0].readyInMinutes}</i>
           <i className="fa fa-users"> servings: {oneRecipeData[0].servings}</i>
-          <img src={oneRecipeData[0].image}></img>
+          <img src={oneRecipeData[0].image} class="img-fluid" alt="..."></img>
           <div className="stars">
             <ul>
               {igredients.map((el) => <li>{el}</li>)}
@@ -20,6 +21,7 @@ function Recipe({ oneRecipeData,igredients , username, userid}) {
           <p className="info">
             {oneRecipeData[0].instructions}
           </p>
+        </div>
         </div>
     </Layout>
   );
