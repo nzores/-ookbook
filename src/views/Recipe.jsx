f35649fb11c05cfc36a2b8a83e731182fca7c3a1
EdDja <serb90@mail.ru>
2023-02-23 12:15:13 +0300

    Card1

--- src/views/Recipe.jsx
+++ src/views/Recipe.jsx
@@ -6,20 +6,47 @@ function Recipe(props) {
   const { plainCoockbooks: coockbook } = props;
   return (
     <Layout>
-      <div className="card" style={{ width: "30rem", height: "20rem" }}>
-        <img className="card-img-top" src="https://kartinkin.net/uploads/posts/2021-04/1617232289_11-p-zharenaya-kurochka-krasivo-16.jpg" alt="Card image cap" />
-        <div className="card-body">
-          <h5 className="card-title">Chiken</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
-          <button style={{ width: "5rem", height: "5rem" }} className="btn btn-block btn-primary"><i className="fa fa-thumbs-up">Like</i></button>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+      <script defer src="css/style.css" />
+      <div className="card">
+        <div className="header">
+          <div className="icon">
+            <a href="#">
+              <i className="fa fa-heart-o" />
+            </a>
+          </div>
         </div>
+        <div className="text">
+          <h1 className="food">Chinese Noodles</h1>
+          <i className="fa fa-clock-o"> 15 Mins</i>
+          <i className="fa fa-users"> Serves 2</i>
+
+          <div className="stars">
+            <li>
+              <a href="#">
+                <i className="fa fa-star" />
+              </a>
+              <a href="#">
+                <i className="fa fa-star" />
+              </a>
+              <a href="#">
+                <i className="fa fa-star" />
+              </a>
+              <a href="#">
+                <i className="fa fa-star" />
+              </a>
+              <a href="#">
+                <i className="fa fa-star-o" />
+              </a>
+            </li>
+          </div>
+          <p className="info">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
+            temporibus.
+          </p>
+        </div>
+        <a href="#" className="btn">
+          Let's Cook!
+        </a>
       </div>
     </Layout>
   );
