ab9158164f32987c6732e421020503d109cbf246
sucelius-thinkPad <sucelius@yandex.ru>
2023-02-23 11:48:04 +0300

    data in ricipe page

--- src/controllers/recipeControllers.js
+++ src/controllers/recipeControllers.js
@@ -1,7 +1,51 @@
+const e = require("express");
 const render = require("../lib/renderTemplate");
-
+const fs = require('fs').promises;
 const RecipePage = require("../views/Recipe");
 
-exports.Recipe = (req, res) => {
-  render(RecipePage, {}, res);
+exports.Recipe = async (req, res) => {
+  const userid = req.session?.user?.id;
+
+
+//   const myApiKey = process.env.MY_API_KEY;
+//   // тут нужно указать сколько рецептов мы хотим получить за один запрос
+// const dishId = 10
+//   // запрос к апи
+
+//   const response = await fetch(` https://api.spoonacular.com/recipes/${dishId}/information?includeNutrition=false&apiKey=${myApiKey}`, {
+//     method: 'GET',
+//     headers: {'Content-Type': 'application/json'}
+//   });
+  
+  let obj;
+  const file = fs.readFile(
+    './src/controllers/response-recipes1-example.json',
+    'utf8',
+    (err, data) => {
+      // console.log('data: ', data);
+      if (err) throw err;
+      obj = data;
+    }
+  );
+  const newObj = JSON.parse(await file);
+
+
+    console.log('=>>>>>>>>>>.', newObj)
+    const igredients = newObj.recipes[0].extendedIngredients.map((el) => el.original)
+
+  const oneRecipeData = newObj.recipes.map((el) => {
+    return(
+      {
+        id:el.id,
+        title:el.title,
+        readyInMinutes:el.readyInMinutes,
+        servings:el.servings,
+        image:el.image,
+        instructions:el.instructions
+      }
+    )
+  })
+ 
+
+  render(RecipePage, {oneRecipeData, igredients}, res);
 };
--- src/views/Recipe.jsx
+++ src/views/Recipe.jsx
@@ -2,8 +2,8 @@ const React = require("react");
 
 const Layout = require("./Layout");
 
-function Recipe(props) {
-  const { plainCoockbooks: coockbook } = props;
+function Recipe({ oneRecipeData,igredients}) {
+  // const { oneRecipeData,igredients} = props;
   return (
     <Layout>
       <script defer src="css/style.css" />
@@ -16,32 +16,17 @@ function Recipe(props) {
           </div>
         </div>
         <div className="text">
-          <h1 className="food">Chinese Noodles</h1>
-          <i className="fa fa-clock-o"> 15 Mins</i>
-          <i className="fa fa-users"> Serves 2</i>
-
+          <h1 className="food">{oneRecipeData[0].title}</h1>
+          <i className="fa fa-clock-o">cooking time : { oneRecipeData[0].readyInMinutes}</i>
+          <i className="fa fa-users"> servings: {oneRecipeData[0].servings}</i>
+          <img src={oneRecipeData[0].image}></img>
           <div className="stars">
-            <li>
-              <a href="#">
-                <i className="fa fa-star" />
-              </a>
-              <a href="#">
-                <i className="fa fa-star" />
-              </a>
-              <a href="#">
-                <i className="fa fa-star" />
-              </a>
-              <a href="#">
-                <i className="fa fa-star" />
-              </a>
-              <a href="#">
-                <i className="fa fa-star-o" />
-              </a>
-            </li>
+            <ul>
+              {igredients.map((el) => <li>{el}</li>)}
+            </ul>
           </div>
           <p className="info">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
-            temporibus.
+            {oneRecipeData[0].instructions}
           </p>
         </div>
         <a href="#" className="btn">
