2e35d52da34b33e53b3c7d5fb0bc83bade731627
sucelius-thinkPad <sucelius@yandex.ru>
2023-02-24 13:08:09 +0300

    iam tired

--- public/js/application.js
+++ public/js/application.js
@@ -25,8 +25,7 @@ const sortBtnParentFav = document.getElementById("sortButtonsFav");
 const recipesContainer = document.getElementById("containerRecipes");
 const recipesContainerFav = document.getElementById("containerRecipesFav");
 
-const recipesContainer = document.getElementById('containerRecipes');
-const recipesContainerFav = document.getElementById('containerRecipesFav');
+
 
 const upArrow = 'M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z'
 const downArrow = 'M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z'
@@ -100,9 +99,7 @@ recipesContainer?.addEventListener("click", async (event) => {
 
 sortBtnParentFav?.addEventListener("click", async (event) => {
   while (recipesContainerFav.firstChild) {
-    recipesContainerFav.removeChild(recipesContainerFav.firstChild);
-  }
-  if (event.target.id === "sortByIngredientsFav") {
+    recipesContainerFav.removeChild(recipesContainerFav.firstChild);nzores
     const response = await fetch("/recipes/showlist/sortByIngredientsFav");
     const sortedRecipes = await response.json();
     const { sortedByIngredients, recipesFav, username } = sortedRecipes;
