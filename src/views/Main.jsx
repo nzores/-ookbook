be0273c725816656f17584b3853a27445452898d
EdDja <serb90@mail.ru>
2023-02-24 12:22:29 +0300

    img

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,11 +1,9 @@
 /* eslint-disable no-nested-ternary */
-const React = require('react');
+const React = require("react");
 
-const Layout = require('./Layout');
+const Layout = require("./Layout");
 
-function Main({
-  username, recipes, userid, recipesFav,
-}) {
+function Main({ username, recipes, userid, recipesFav }) {
   return (
     <Layout username={username} userid={userid}>
       <h1>Main Page</h1>
@@ -14,26 +12,15 @@ function Main({
         <button id="sortByCooking">sort by cooking time</button>
       </div>
 
-      <div id="containerRecipes">
-        {recipes &&
-          recipes.map((el) => (
-            <div key={el.recipeId} data-recipe={el.recipeId}>
-              <h2>{el.name}</h2>
-              <h2>count ingrediants {el.ingredientsCount} unit/s</h2>
-              <h2>cooking time {el.cookingTime} min</h2>
-              <h3>{el.image}</h3>
-              <a href={`/recipe/${el.recipeId}`}>click</a>
-            </div>
-          ))}
-     <div className="container text-center">
+      <div className="container text-center">
         <div className="row align-items-start" id="containerRecipes">
-          {recipes
-            && recipes.map((el) => (
+          {recipes &&
+            recipes.map((el) => (
               <div className="col-sm">
                 <div
                   className="card"
                   key={el.recipeId}
-                  style={{ width: '18rem' }}
+                  style={{ width: "18rem" }}
                 >
                   <img src={el.image} className="card-img-top" alt="" />
                   <div
@@ -45,20 +32,8 @@ function Main({
                     <a href={`/recipe/${el.recipeId}`}>
                       <h2 className="card-title">{el.name}</h2>
                     </a>
-                    <p>
-                      Count ingrediants
-                      {' '}
-                      {el.ingredientsCount}
-                      {' '}
-                      unit/s
-                    </p>
-                    <p>
-                      Cooking time
-                      {' '}
-                      {el.cookingTime}
-                      {' '}
-                      min
-                    </p>
+                    <p>Count ingrediants {el.ingredientsCount} unit/s</p>
+                    <p>Cooking time {el.cookingTime} min</p>
                     {/* <button type="button" className="btn btn-outline-primary">
                       Unlike
                     </button> */}
@@ -66,7 +41,7 @@ function Main({
 
                     {username ? (
                       recipesFav.find(
-                        (elem) => elem.recipeId === el.recipeId,
+                        (elem) => elem.recipeId === el.recipeId
                       ) ? (
                         <>
                           <svg className="heart" viewBox="0 0 256 256">
@@ -82,24 +57,24 @@ function Main({
                             {/* Удалить из Избранного */}
                           </div>
                         </>
-                        ) : (
-                          <>
-                            <svg className="heart" viewBox="0 0 256 256">
-                              <path
-                                id="favPath"
-                                d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
-                                strokeWidth="20px"
-                                stroke="#FFF"
-                                fill="none"
-                              />
-                            </svg>
-                            <div className="favInput" data-fav="false">
-                              {/* Добавить в Избранное */}
-                            </div>
-                          </>
-                        )
+                      ) : (
+                        <>
+                          <svg className="heart" viewBox="0 0 256 256">
+                            <path
+                              id="favPath"
+                              d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                              strokeWidth="20px"
+                              stroke="#FFF"
+                              fill="none"
+                            />
+                          </svg>
+                          <div className="favInput" data-fav="false">
+                            {/* Добавить в Избранное */}
+                          </div>
+                        </>
+                      )
                     ) : (
-                      ''
+                      ""
                     )}
                     {/* <button
                       type="button"
@@ -121,7 +96,6 @@ function Main({
         {/* <div id="spinner" className="spinner">
           <div />
         </div> */}
-      </div>
       </div>
     </Layout>
   );
